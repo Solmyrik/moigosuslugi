dc0831204caf064ac0a5863dbdb1922fda4e0f43
C:\Users\Robik\Desktop\Проекты\React\solmlink <schedamian@gmail.com>
2023-06-16 16:30:09 +0300

    add fix main

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
-//select
-const values = [
-  {
-    value: 'En',
-    label: '<img src="img/countries/ru1.svg"/>',
-    id: 2,
-  },
-  {
-    value: 'Ru',
-    label: '<img src="img/countries/ru.svg"/>',
-    id: 1,
-    selected: true,
-  },
-];
-const defaultSelect = () => {
-  const element = document.querySelectorAll('.select-d');
-  element.forEach((e, i) => {
-    const choices = new Choices(e, {
-      choices: values,
-      // searchEnabled: true,
-      // searchPlaceholderValue: 'Поиск',
-      placeholder: true,
-      searchEnabled: false,
-      searchPlaceholderValue: '',
-      searchChoices: false,
-      // removeItemButton: true,
-    });
-  });
-};
+// //select
+// const values = [
+//   {
+//     value: 'En',
+//     label: '<img src="img/countries/ru1.svg"/>',
+//     id: 2,
+//   },
+//   {
+//     value: 'Ru',
+//     label: '<img src="img/countries/ru.svg"/>',
+//     id: 1,
+//     selected: true,
+//   },
+// ];
+// const defaultSelect = () => {
+//   const element = document.querySelectorAll('.select-d');
+//   element.forEach((e, i) => {
+//     const choices = new Choices(e, {
+//       choices: values,
+//       // searchEnabled: true,
+//       // searchPlaceholderValue: 'Поиск',
+//       placeholder: true,
+//       searchEnabled: false,
+//       searchPlaceholderValue: '',
+//       searchChoices: false,
+//       // removeItemButton: true,
+//     });
+//   });
+// };
 
-defaultSelect();
+// defaultSelect();
 
 //
-const phone = document.querySelector('.phone');
-const phoneSelect = document.querySelector('.select-phone');
+// const phone = document.querySelector('.phone');
+// const phoneSelect = document.querySelector('.select-phone');
 
-phoneSelect.addEventListener('change', (e) => {
-  if (e.currentTarget.value === 'En') {
-    phone.value = '+1';
-  }
-  if (e.currentTarget.value === 'Ru') {
-    phone.value = '+7';
-  }
-});
+// phoneSelect.addEventListener('change', (e) => {
+//   if (e.currentTarget.value === 'En') {
+//     phone.value = '+1';
+//   }
+//   if (e.currentTarget.value === 'Ru') {
+//     phone.value = '+7';
+//   }
+// });
 
 // change__body
 const changeBody = document.querySelectorAll('.change__body');
